2dee18969395cc5e012e6f515be979b60468c283
Kolesias123 <webmaster@infosmart.mx>
2012-07-28 06:23:43 -0500

    BeatRock v2.4.4 - /* Mejora del sistema de DEBUG */

--- Kernel/Headers/Footer.php
+++ Kernel/Headers/Footer.php
@@ -15,7 +15,7 @@ if(!defined('BEATROCK'))
 	
 if(defined('DEBUG') AND DEBUG == true)
 {
-	echo '<div class="wrapper" style="font-size: 11px; margin-top: 20px; line-height: 15px;"><hr />';
+	echo '<div class="wrapper" style="font-size: 11px; margin-top: 20px; line-height: 15px;"><hr /><br />';
 	echo BitRock::Statistics();
 	echo '<br />* Esta información aparece debido a que tiene definida la constante DEBUG en Init.php';
 	echo '</div>';
