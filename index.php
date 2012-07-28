e210dd93d8c995062db565d1f5aceec8047b93e4
Kolesias123 <webmaster@infosmart.mx>
2012-07-28 06:26:37 -0500

    BeatRock v2.4.4 - /* Mejorado */

--- index.php
+++ index.php
@@ -5,5 +5,7 @@ $hello = 'Hola querido mundo';
 
 $page['id'] 			= 'index';
 $page['name'] 			= 'Inicio';
+
+$page['lang'] 			= (!empty($G['lang'])) ? $G['lang'] : LANG;
 $page['lang.sections'] 	= array('page.welcome');
 ?>
