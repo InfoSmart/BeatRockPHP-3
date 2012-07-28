c27471e3f9d239ca29df64db0263c2f36ded7ff8
Kolesias123 <webmaster@infosmart.mx>
2012-07-28 06:26:29 -0500

    BeatRock v2.4.4 - /* Mejorado */

--- Init.php
+++ Init.php
@@ -26,6 +26,9 @@
 ## PREPARACIÓN DE CONSTANTES Y OPCIONES INTERNAS	
 #############################################################
 
+// Reporte de errores recomendado para empezar.
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+
 // Permitir acciones internas.
 define('BEATROCK', 	true);
 define('START', 	microtime(true));
@@ -33,9 +36,9 @@ define('START', 	microtime(true));
 
 // Información esencial del usuario.
 define('IP', 	$_SERVER['REMOTE_ADDR']);
-define('AGENT', @$_SERVER['HTTP_USER_AGENT']);
-define('FROM', 	@$_SERVER['HTTP_REFERER']);
-define('LANG', 	@substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
+define('AGENT', $_SERVER['HTTP_USER_AGENT']);
+define('FROM',  $_SERVER['HTTP_REFERER']);
+define('LANG', 	substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
 
 // Dirección actual y uso del protocolo seguro.
 define('URL', $_SERVER['SERVER_NAME'] . $_SERVER["REQUEST_URI"]);
@@ -52,6 +55,7 @@ define('TEMPLATES_BIT', TEMPLATES . 'bitrock' . DS);
 define('HEADERS', 		KERNEL . 'Headers' . DS);
 define('BIT', 			KERNEL . 'BitRock' . DS);
 define('MODS', 			KERNEL . 'Modules' . DS);
+define('SITE_MODS', 	MODS . 'Site' . DS);
 define('LANGUAGES', 	KERNEL . 'Languages' . DS);
 
 // Ajustando configuración de PHP recomendada.
@@ -59,8 +63,8 @@ ini_set('zlib.output_compression', 	'Off');
 ini_set('short_open_tag', 			'On');
 ini_set('expose_php', 				'Off');
 
-// Reporte de errores predeterminado.
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+// Activando el colector de referencia circular.
+gc_enable();
 
 // Empezar sesión.
 session_start();
@@ -129,7 +133,7 @@ else if(!$config['server']['backup'])
 ## FUNCIONES DE ACCESO DIRECTO
 #############################################################
 
-// Función - Remplazar "accesos directos" en una cadena.
+// Remplazar "accesos directos" en una cadena.
 // - $str: 	Cadena de texto a ajustar.
 // - $other (array): Otros parametros a reemplazar.
 function ShortCuts($str, $other = '')
@@ -149,14 +153,14 @@ function ShortCuts($str, $other = '')
 	return $str;
 }
 
-// Función - Ejecutar consulta en el servidor MySQL.
+// Ejecutar consulta en el servidor MySQL.
 // - $q: Consulta a ejecutar.
 function query($q)
 {
 	return MySQL::query($q);
 }
 
-// Función - Insertar datos en la base de datos.
+// Insertar datos en la base de datos.
 // - $table: Tabla a insertar los datos.
 // - $data (Array): Datos a insertar.
 function query_insert($table, $data)
@@ -168,7 +172,7 @@ function Insert($table, $data)
 	return MySQL::query_insert($table, $data);
 }
 
-// Función - Actualizar datos en la base de datos.
+// Actualizar datos en la base de datos.
 // - $table: Tabla a insertar los datos.
 // - $updates (Array): Datos a actualizar.
 // - $where (Array): Condiciones a cumplir.
@@ -182,7 +186,7 @@ function Update($table, $updates, $where = '', $limit = 1)
 	return MySQL::query_update($table, $updates, $where, $limit);
 }
 
-// Función - Obtener numero de valores de una consulta MySQL.
+// Obtener numero de valores de una consulta MySQL.
 // - $q: Consulta a ejecutar.
 function query_rows($q)
 {
@@ -193,14 +197,18 @@ function Rows($q)
 	return MySQL::query_rows($q);
 }
 
-// Función - Obtener los valores de una consulta MySQL.
+// Obtener los valores de una consulta MySQL.
 // - $q: Consulta a ejecutar.
+function query_assoc($q)
+{
+	return MySQL::query_assoc($q);
+}
 function Assoc($q)
 {
 	return MySQL::query_assoc($q);
 }
 
-// Función - Obtener un dato especifico de una consulta MySQL.
+// Obtener un dato especifico de una consulta MySQL.
 // - $q: Consulta a ejecutar.
 function query_get($q, $row)
 {
@@ -211,21 +219,48 @@ function Get($q)
 	return MySQL::query_get($q);
 }
 
-// Función - Obtener numero de valores de un recurso MySQL o la última consulta hecha.
+// Obtener numero de valores de un recurso MySQL o la última consulta hecha.
 // - $q: Recurso de una consulta.
 function num_rows($q = '')
 {
 	return MySQL::num_rows($q);
 }
 
-// Función - Obtener los valores de un recurso MySQL o la última consulta hecha.
+// Obtener los valores de un recurso MySQL o la última consulta hecha.
 // - $q: Recurso de la consulta.
 function fetch_assoc($q = '')
 {
 	return MySQL::fetch_assoc($q);
 }
 
-// Función - Filtrar una cadena para evitar Inyección SQL.
+// Obtener los valores de un recurso MySQL o la última consulta hecha.
+// - $q: Recurso de la consulta.
+function fetch_object($q = '')
+{
+	return MySQL::fetch_object($q);
+}
+
+// Obtener los valores de un recurso MySQL o la última consulta hecha.
+// - $q: Recurso de la consulta.
+function fetch_array($q = '')
+{
+	return MySQL::fetch_array($q);
+}
+
+// Liberar la memoria de la última consulta realizada.
+// - $q: Recurso de la consulta.
+function free_result($q = '')
+{
+	return MySQL::free_result($q);
+}
+
+// Obtener la última ID insertada en la base de datos.
+function last_id()
+{
+	return MySQL::last_id();
+}
+
+// Filtrar una cadena para evitar Inyección SQL.
 // - $str: Cadena a filtrar.
 // - $html (Bool): ¿Filtrar HTML con HTML ENTITIES? (Evitar Inyección XSS)
 // - $e (Charset): Codificación de letras de la cadena a filtrar.
@@ -238,7 +273,7 @@ function _f($str, $html = true, $e = 'ISO-8859-15')
 	return Core::FilterText($str, $html, $e);
 }
 
-// Función - Filtrar una cadena para evitar Inyección XSS.
+// Filtrar una cadena para evitar Inyección XSS.
 // - $str: Cadena a filtrar.
 // - $e (Charset): Codificación de letras de la cadena a filtrar.
 function CleanText($str, $e = 'ISO-8859-15')
@@ -250,7 +285,7 @@ function _c($str, $e = 'ISO-8859-15')
 	return Core::CleanText($str, $e);
 }
 
-// Función - Búsqueda de un término en una cadena.
+// Búsqueda de un término en una cadena.
 // - $str: Cadena donde buscar.
 // - $words (Cadena/Array): Término(s) a buscar.
 // - $lower (Bool): ¿Convertir todo a minusculas?
@@ -259,7 +294,7 @@ function Contains($str, $words, $lower = false)
 	return Core::Contains($str, $words, $lower);
 }
 
-// Función - Obtener la fecha actual en caracteres.
+// Obtener la fecha actual en caracteres.
 // - $hour (Bool): ¿Incluir hora?
 // - $type (1, 2, 3): Modo de respuesta.
 function NormalDate($hour = true, $type = 1)
@@ -280,7 +315,7 @@ function NormalDate($hour = true, $type = 1)
 	return $date;
 }
 
-// Función - Obtener el mes en caracteres de un mes numerico.
+// Obtener el mes en caracteres de un mes numerico.
 // - $num (Int): Mes numerico.
 // - $c (Bool): ¿Obtener solo las tres primeras letras?
 function GetMonth($num, $c = false)
@@ -288,7 +323,7 @@ function GetMonth($num, $c = false)
 	return Core::GetMonth($num, $c);
 }
 
-// Función - Imprimir correctamente un Array;
+// Imprimir de manera visual una matriz (array);
 // - $a (Array): Array a imprimir.
 function _r($a)
 {
@@ -300,12 +335,17 @@ function _r($a)
 	echo '</pre>';
 }
 
-function _l($data, $section = '')
+// Traducir una cadena.
+// - $data: Cadena.
+// - $section: Nombre de la sección a utilizar.
+// - $lang: Lenguaje a traducir.
+// - $tpl: ¿Preparado para la traducción en tiempo real?
+function _l($data, $section = '', $lang = '', $tpl = false)
 {
-	return Lang::SetParams($data, $section);
+	return Lang::SetParams($data, $lang, $section, $tpl);
 }
 
-// Función - Calcular tiempo restante/faltante.
+// Calcular tiempo restante/faltante.
 // - $date: Tiempo Unix o cadena de tiempo.
 // - $num: Devolver solo el numero y tipo.
 function CalcTime($date, $num = false)
@@ -313,7 +353,7 @@ function CalcTime($date, $num = false)
 	return Core::CalculateTime($date, $num);
 }
 
-// Función - Guardar log.
+// Guardar log.
 // - $message: Mensaje a guardar.
 // - $type (info, warning, error, mysql): Tipo del log.
 function Reg($message, $type = 'info')
@@ -321,6 +361,7 @@ function Reg($message, $type = 'info')
 	BitRock::Log($message, $type);
 }
 
+// Borrar todas las cookies.
 function cookie_destroy()
 {
 	foreach($_COOKIE as $param)
@@ -328,6 +369,43 @@ function cookie_destroy()
 		setcookie($param, '', -1000);
 		unset($_COOKIE[$param]);
 	}
+}
+
+// Definir una sesión.
+// - $param: Parametro/Nombre.
+// - $value: Valor, si se deja vacio se retornara el valor actual.
+function _SESSION($param, $value = '')
+{
+	return Core::theSession($param, $value);
+}
+
+// Eliminar una sesión.
+// - $param: Parametro/Nombre.
+function _DELSESSION($param)
+{
+	return Core::delSession($param);
+}
+
+// Definir una cookie.
+// - $param: Parametro/Nombre.
+// - $value: Valor, si se deja vacio se retornara el valor actual.
+// - $duration: Duración en segundos.
+// - $path: Ubicación donde podrá ser válida.
+// - $domain: Dominio donde podrá ser válida.
+// - $secure (Bool): ¿Solo válida para HTTPS?
+// - $imgod (Bool): Si se activa, el navegador web no podrá acceder a la cookie. (Como por ejemplo en JavaScript)
+function _COOKIE($param, $value = '', $duration = '', $path = '', $domain = '', $secure = false, $imgod = false)
+{
+	return Core::theCookie($param, $value, $duration, $path, $domain, $secure, $imgod);
+}
+
+// Eliminar una cookie.
+// - $param: Parametro/Nombre.
+// - $path: Ubicación donde es válida.
+// - $domain: Dominio donde es válida.
+function _DELCOOKIE($param, $path = '', $domain = '')
+{
+	return Core::delCookie($param, $path, $domain);
 }
 
 #############################################################
