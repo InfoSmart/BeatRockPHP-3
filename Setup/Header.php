e8dc4e44cfd460eb5794fa7144bff227ffa302a5
Kolesias123 <webmaster@infosmart.mx>
2012-07-29 13:17:55 -0500

    BeatRock v2.4.4 - /* Mejoras */

--- Setup/Header.php
+++ Setup/Header.php
@@ -19,13 +19,13 @@ define('RESOURCES_INS', '//resources.infosmart.mx');
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>Instalación de BeatRock</title>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	
 	<meta charset="iso-8859-15" />
 
-	<link rel="icon" href="http://resources.infosmart.mx/beatrock/images/favicon.ico" />
-	<link rel="shortcut icon" href="http://resources.infosmart.mx/beatrock/images/favicon.ico" type="image/vnd.microsoft.icon" />
+	<title>Instalación de BeatRock</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+	<link rel="icon" href="//resources.infosmart.mx/beatrock/images/favicon.ico" />
+	<link rel="shortcut icon" href="//resources.infosmart.mx/beatrock/images/favicon.ico" type="image/vnd.microsoft.icon" />
 
 	<meta name="publisher" content="InfoSmart." />
 	<meta name="copyright" content="© 2012 InfoSmart. Todos los derechos reservados. http://beatrock.infosmart.mx/">
@@ -33,7 +33,7 @@ define('RESOURCES_INS', '//resources.infosmart.mx');
 	<meta name="robots" content="noodp, nofollow, noindex" />
 	
 	<script>
-	Path = "<?php if(defined("PATH")) { echo PATH . '/Setup'; } else { echo '.'; } ?>",
+	Path = "<? if(defined("PATH")) { echo PATH . '/Setup'; } else { echo '.'; } ?>",
 	Resources_Sys = "<?=RESOURCES_INS?>/systemv2";
 	</script>
 	
