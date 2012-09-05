7d1b221837c32f01392973fe7dd4d179d6fed715
Kolesias123 <webmaster@infosmart.mx>
2012-09-05 00:00:31 -0500

    BeatRock v2.4.5 - Optimización.

--- Kernel/Headers/Header.php
+++ Kernel/Headers/Header.php
@@ -1,68 +1,18 @@
-<?php
-#####################################################
-## 					 BeatRock				   	   ##
-#####################################################
-## Framework avanzado de procesamiento para PHP.   ##
-#####################################################
-## InfoSmart © 2012 Todos los derechos reservados. ##
-## http://www.infosmart.mx/						   ##
-#####################################################
-## http://beatrock.infosmart.mx/				   ##
-#####################################################
-
-if(!defined('BEATROCK'))
-	exit;
-
-#####################################################
-## META ETIQUETAS RECOMENDADAS	
-#####################################################
-
-if(empty($page['image']) AND defined('LOGO'))
-	$page['image'] = LOGO;
-
-Tpl::addMeta('og:title', 		$site['name'], 'property');
-Tpl::addMeta('og:site_name', 	SITE_NAME, 'property');
-Tpl::addMeta('og:url', 			PATH_NOW, 'property');
-Tpl::addMeta('og:description', 	$site['site_description'], 'property');
-Tpl::addMeta('og:type', 		$site['site_type'], 'property');
-
-if(!empty($page['image']))
-{
-	Tpl::addMeta('og:image', 	$page['image'], 'property');
-	Tpl::addMeta('image', 		$page['image'], 'itemprop');
-}
-
-if(!empty($site['site_locale']))
-	Tpl::addMeta('og:locale', $site['site_locale'], 'property');
-
-if(!empty($site['site_og']))
-{
-	$og = json_decode(utf8_encode($site['site_og']), true);
-
-	foreach($og as $param => $value)
-		Tpl::addMeta($param, Core::FixText(html_entity_decode($value)), 'property');
-}
-
-$hlang = Lang::PrepareLive();
-$hlang = json_encode($hlang);
-
-Tpl::addMeta('name', $site['name'], 'itemprop');
-Tpl::addMeta('description', $site['site_description'], 'itemprop');
-?>
 <!DOCTYPE html>
 <html lang="%site_language%" <?=Tpl::$mhtml?>>
 <head <?=Tpl::$mhead?>>
 	<meta charset="%site_charset%" />
+	
 	<title><?=$site['name']?></title>
 	<meta name="title" content="<?=$site['name']?>" />
 	
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta name="lang" content="%site_language%" />
 	
-	<?php if(!empty($site['site_favicon'])) { ?>
+	<? if(!empty($site['site_favicon'])) { ?>
 	<link rel="icon" href="%RESOURCES%/images/%site_favicon%" />
 	<link rel="shortcut icon" href="%RESOURCES%/images/%site_favicon%" type="image/vnd.microsoft.icon" />
-	<?php } ?>
+	<? } ?>
 	
 	<meta name="application-name" content="%SITE_NAME%" />
 	<meta name="application-url" content="%PATH%" />
@@ -79,15 +29,15 @@ Tpl::addMeta('description', $site['site_description'], 'itemprop');
 	<meta name="publisher" content="%site_publisher%" />
 	<meta name="copyright" content="© <?=$date['Y']?> %site_publisher%" />
 	
-<?php
+<?
 echo Tpl::$metas, "\r\n";
 echo Tpl::$stuff;
 ?>
 	
-	<?php if($page['cache']) { ?>
+	<? if($page['cache']) { ?>
 	<meta http-equiv="expires" content="-1" />
 	<meta http-equiv="cache-control" content="private max-age=0" />
-	<?php } ?>
+	<? } ?>
 	
 	<link rel="canonical" href="%PATH%/" />
 	
@@ -108,10 +58,10 @@ echo Tpl::$stuff;
 <?=Tpl::$vars?>
 	</script>
 	
-	<?php
-	echo "<!-- Estilos -->\r\n" . Tpl::$styles;
+	<?
+	echo "<!-- CSS -->\r\n" . Tpl::$styles;
 
-	if($site['site_bottom_javascript'] !== "true")
+	if($site['site_bottom_javascript'] !== 'true')
 		echo "	<!-- JavaScript -->\r\n" . Tpl::$js;
 
 	if(!empty($site['site_header_javascript']))
@@ -119,4 +69,3 @@ echo Tpl::$stuff;
 	?>	
 </head>
 <body>
-	<div class="page" id="page">
