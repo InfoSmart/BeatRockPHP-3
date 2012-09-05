efe80b95202b0719634a4f8de17c7eb50d989ecb
Kolesias123 <webmaster@infosmart.mx>
2012-09-05 00:04:22 -0500

    BeatRock v2.4.5 - Estructura de código más limpia.

--- Kernel/Headers/SubHeader.php
+++ Kernel/Headers/SubHeader.php
@@ -1,19 +1,4 @@
-<?php
-#####################################################
-## 					 BeatRock				   	   ##
-#####################################################
-## Framework avanzado de procesamiento para PHP.   ##
-#####################################################
-## InfoSmart © 2012 Todos los derechos reservados. ##
-## http://www.infosmart.mx/						   ##
-#####################################################
-## http://beatrock.infosmart.mx/				   ##
-#####################################################
-
-// Acción ilegal.
-if(!defined('BEATROCK'))
-	exit;
-?>
+<div class="page" id="page">
 <header>
 	<div class="wrapper">
 		<h1>%welcome%</h1>
