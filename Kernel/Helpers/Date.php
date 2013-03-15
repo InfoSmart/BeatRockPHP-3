7734f9cac922fe07fa10ed643b6528e083024979
Kolesias123 <webmaster@infosmart.mx>
2013-03-15 11:35:17 -0600

    BeatRock 3.0.2 - Código más limpio, se han arreglado problemas con el lenguaje.

--- Kernel/Helpers/Date.php
+++ Kernel/Helpers/Date.php
@@ -1,119 +1,183 @@
 <?
-#####################################################
-## 					 BeatRock				   	   ##
-#####################################################
-## Framework avanzado de procesamiento para PHP.   ##
-#####################################################
-## InfoSmart ? 2012 Todos los derechos reservados. ##
-## http://www.infosmart.mx/						   ##
-#####################################################
-## http://beatrock.infosmart.mx/				   ##
-#####################################################
+/**
+ * BeatRock
+ *
+ * Framework para el desarrollo de aplicaciones web.
+ *
+ * @author 		Iván Bravo <webmaster@infosmart.mx> @Kolesias123
+ * @copyright 	InfoSmart 2013. Todos los derechos reservados.
+ * @license 	http://creativecommons.org/licenses/by-sa/2.5/mx/  Creative Commons "Atribución-Licenciamiento Recíproco"
+ * @link 		http://beatrock.infosmart.mx/
+ * @version 	3.0
+ *
+ * @package 	Date
+ * Contiene funciones para el procesamiento de fechas.
+ *
+*/
 
-// Acci?n ilegal.
-if(!defined('BEATROCK'))
+# Acción ilegal.
+if ( !defined('BEATROCK') )
 	exit;
+
+const 	DASH 	= 1,
+		SLASH 	= 2,
+		THE 	= 3;
 
 class Date
 {
-	// Convertir tiempo Unix a tiempo en cadena.
-	// - $time (Int): Tiempo Unix.
-	// - $hour (Bool): ¿Incluir hora?
-	// - $type (1, 2, 3): Tipo de separaci?n.
-	static function TimeDate($time = '', $hour = false, $type = 1)
+	/**
+	 * Convierte un tiempo unix en una fecha con formato.
+	 * @param string  $time Tiempo unix
+	 * @param boolean $hour ¿Incluir hora?
+	 * @param integer $type Tipo de separación deseada.
+	 */
+	static function TimeDate($time = '', $hour = false, $type = DASH)
 	{
-		if(!is_numeric($time))
+		# No hay tiempo definido, usar el tiempo unix actual.
+		if ( !is_numeric($time) )
 			$time = time();
-			
-		if(!is_numeric($type) OR $type < 1 OR $type > 3)
-			$type = 1;
-		
-		if($type == 1)
-			$date = date('d', $time) . '-' . self::GetMonth(date('m', $time)) . '-' . date('Y', $time);
 
-		if($type == 2)
-			$date = date('d', $time) . '/' . self::GetMonth(date('m', $time)) . '/' . date('Y', $time);
-
-		if($type == 3)
+		switch ( $type )
 		{
-			if(LANG == 'en')
-				$date = self::GetMonth(date('m', $time)) . ' ' . date('d', $time) . ', ' . date('Y', $time);
-			else
-				$date = date('d', $time) . ' %the% ' . ucwords(self::GetMonth(date('m', $time))) . ' %the% ' . date('Y', $time);
+			# Separación por -
+			case DASH:
+			default:
+				$date = date('d', $time) . '-' . self::GetMonth(date('m', $time)) . '-' . date('Y', $time);
+			break;
 
-			$date = _l($date, 'global');
+			# Separación por /
+			case SLASH:
+				$date = date('d', $time) . '/' . self::GetMonth(date('m', $time)) . '/' . date('Y', $time);
+			break;
+
+			# Separación por la palabra "de"
+			case THE:
+				# El Inglés tiene su caso especial.
+				if ( LANG == 'en' )
+					$date = self::GetMonth(date('m', $time)) . ' ' . date('d', $time) . ', ' . date('Y', $time);
+				else
+					$date = date('d', $time) . ' %the% ' . ucwords(self::GetMonth(date('m', $time))) . ' %the% ' . date('Y', $time);
+
+				$date = _l($date, 'global');
+			break;
 		}
-		
-		if($hour)
-			$date .= ' - ' . date('H:i:s', $time);
-			
+
+		# Incluir hora.
+		if ( $hour )
+			$date .= ' ' . date('H:i:s', $time);
+
 		return $date;
 	}
 
-	// Calcular tiempo restante/faltante.
-	// - $date: Tiempo Unix o cadena de tiempo.
-	// - $num: Devolver solo el numero y tipo.
-	static function CalculateTime($date, $num = false)
+	/**
+	 * Calcula el tiempo restante/faltante para llegar al tiempo especificado.
+	 * @param mixed  $date  	Tiempo unix o cadena de tiempo.
+	 * @param boolean $onlyNum  ¿Devolver solo la cantidad?
+	 */
+	static function CalculateTime($date, $onlyNum = false)
 	{
 		Lang::SetSection('global');
 
 		$int = array('%second%', '%minute%', '%hour%', '%day%', '%week%', '%month%', '%year%');
 		$dur = array(60, 60, 24, 7, 4.35, 12, 12);
-		
-		if(!is_numeric($date))
+
+		# El tiempo no es númerico, intentar transformarlo.
+		if ( !is_numeric($date) )
 			$date = strtotime($date);
-		
+
+		# El formato de tiempo no es válida.
+		if ( !$date )
+			return false;
+
 		$now 	= time();
 		$time 	= $date;
-		
-		if($now > $time)
+
+		# A partir de ahora, calculos matematicos básicos. (Ni dios entiende esto)
+
+		if ( $now > $time )
 		{
 			$dif = $now - $time;
 			$str = '%ago%';
 		}
-		else if($now == $time)
+		else if ( $now == $time )
 			return _l('%now%');
 		else
 		{
 			$dif = $time - $now;
 			$str = '%within%';
 		}
-		
-		for($j = 0; $dif >= $dur[$j] && $j < count($dur) - 1; $j++)
+
+		for ( $j = 0; $dif >= $dur[$j] && $j < count($dur) - 1; ++$j )
 			$dif /= $dur[$j];
-			
+
 		$dif = round($dif);
-		
-		if($dif != 1)
+
+		if ( $dif !== 1 )
 		{
 			$int[5] 	.= "e";
 			$int[$j] 	.= "s";
 		}
-		
-		return ($num) ? _l("$dif") . ' ' .  strtolower(_l("$int[$j]")) : _l("$str $dif") . " " . strtolower(_l("$int[$j]"));
+
+		# FIXME: Esto tiene un problema de codificación...
+		$result = ( $onlyNum ) ? _l("$dif") . ' ' .  _l("$int[$j]") : _l("$str $dif") . " " . _l("$int[$j]");
+		return $result;
 	}
 
-	// Convertir el mes numerico de una fecha a mes en letras.
-	// - $date: Cadena de fecha con separación -, / ? de
-	static function MonthString($date)
+	/**
+	 * Convierte el mes númerico de un formato de fecha o tiempo unix
+	 * en cadena. Es decir 02 lo converteria a Febrero.
+	 * @param mixed $date 		Tiempo unix o formato de fecha (Separación: -, / o "de")
+	 * @param integer $month  	Ubicación desde 0 a 2 en donde se encontraría el mes en un array.
+	 * @param integer $year 	Ubicación desde 0 a 2 en donde se encontraría el año en un array.
+	 * @param integer $day 		Ubicación desde 0 a 2 en donde se encontraría el día en un array.
+	 */
+	static function MonthString($date, $month = 1, $year = 2, $day = 0)
 	{
-		if(is_numeric($date))
+		# Es tiempo unix, sencillo.
+		if ( is_numeric($date) )
 			return self::GetMonth($date);
 
-		if(Contains($date, '-'))
-			$split = explode('-', $date);
+		# La fecha contiene separación por -
+		# Ejemplo: 04-05-1995
+		if ( Contains($date, '-') )
+		{
+			$sep 	= '-';
+			$split 	= explode('-', $date);
+		}
 
-		if(Contains($date, '/'))
-			$split = explode('/', $date);
+		# La fecha contiene separación por /
+		# Ejemplo: 04/05/1995
+		if ( Contains($date, '/') )
+		{
+			$sep 	= '/';
+			$split 	= explode('/', $date);
+		}
 
-		if(Contains($date, _l('%the%', 'global')))
-			$split = explode(_l(' %the% ', 'global'), $date);
-		
-		$month = self::GetMonth($t[1]);		
-		return "$t[0]-$month-$t[2]";
+		# La fecha contiene separación por "de"
+		# Ejemplo: 04 de 05 de 1995
+		if ( Contains($date, _l('%the%', '', 'global')) )
+		{
+
+			$sep 	= _l(' %the% ', '', 'global');
+			$split 	= explode(_l(' %the% ', '', 'global'), $date);
+		}
+
+		# Obtenemos el mes.
+		$newMonth = self::GetMonth($split[$month]);
+
+		# Variable temporal para devolver en el mismo orden que se obtuvo el formato.
+		# FIXME: ¿Hacer algo mejor?
+		$tmp[$month] 	= $newMonth;
+		$tmp[$year] 	= $split[$year];
+		$tmp[$day] 		= $split[$day];
+
+		return $tmp[0] . $sep . $tmp[1] . $sep . $tmp[2];
 	}
 
-	// Obtener una lista (array) de los meses traducidos al idioma del visitante.
+	/**
+	 * Obtiene una lista de los meses traducidos en el idioma del visitante.
+	 * @return array Lista
+	 */
 	static function GetListMonths()
 	{
 		Lang::SetSection('global');
@@ -134,15 +198,17 @@ class Date
 		  '12' => '%december%'
 		);
 
-		foreach($calendar as $key => $value)
+		foreach ( $calendar as $key => $value )
 			$result[$key] = _l($value);
 
 		return $result;
 	}
 
-	// Convertir valor numérico a un mes del año.
-	// - $num (Int): Valor numérico.
-	// - $c (Bool): ¿Retornar todo el mes?
+	/**
+	 * Convierte el valor numérico de un mes en una cadena en el idioma del visitante.
+	 * @param integer $num  Mes numérico.
+	 * @param boolean $c   	¿Retornar el nombre completo del mes? (De otra manera solo devolverá sus 3 primeras letras)
+	 */
 	static function GetMonth($num, $c = false)
 	{
 		Lang::SetSection('global');
@@ -170,50 +236,53 @@ class Date
           '8' 	=> '%august%',
 		  '9' 	=> '%september%'
 		);
-		
-		foreach($calendar as $key => $month)
+
+		foreach ( $calendar as $key => $month )
 		{
-			if(preg_match("/$key/", $num))
+			if ( preg_match("/$key/", $num) )
 			{
 				$month = strtolower(_l($month));
 				return ($c) ? $month : substr($month, 0, 3);
 			}
 		}
-		
+
 		return _l('%unknow%');
 	}
-	
-	// Convertir mes de un a?o a su valor num?rico.
-	// - $name: Mes de a?o.
+
+	/**
+	 * Convierte un mes en cadena a su valor numérico.
+	 * Para mayor compatibilidad entre idiomas, el mes debe ser escrito en inglés.
+	 * @param string $name Mes.
+	 */
 	static function GetMonthNum($name)
 	{
 		Lang::SetSection('global');
 
 		$calendar = array(
-          '01' => '%january%',
-          '02' => '%february%',
-          '03' => '%march%',
-          '04' => '%april%',
-          '05' => '%may%',
-          '06' => '%june%',
-          '07' => '%july%',
-          '08' => '%august%',
-		  '09' => '%september%',
-          '10' => '%october%',
-          '11' => '%november%',
-		  '12' => '%december%'
+          '01' => 'january',
+          '02' => 'february',
+          '03' => 'march',
+          '04' => 'april',
+          '05' => 'may',
+          '06' => 'june',
+          '07' => 'july',
+          '08' => 'august',
+		  '09' => 'september',
+          '10' => 'october',
+          '11' => 'november',
+		  '12' => 'december'
 		);
-		
-		foreach($calendar as $key => $month)
+
+		foreach ( $calendar as $key => $month )
 		{
 			$month = strtolower($month);
 
-			if(preg_match("/$month/i", $name))
+			if ( preg_match("/$month/i", $name) )
 				return _l($key);
-				
+
 			$month = substr($month, 0, 3);
-			
-			if(preg_match("/$month/i", $name))
+
+			if ( preg_match("/$month/i", $name) )
 				return _l($key);
 		}
 
