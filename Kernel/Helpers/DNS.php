177e4d9aea39a0b2352ea01f5fa4097afe857aa0
Kolesias123 <webmaster@infosmart.mx>
2013-03-15 11:34:54 -0600

    BeatRock 3.0.2 - Código más limpio, se ha eliminado una función.

--- Kernel/Helpers/DNS.php
+++ Kernel/Helpers/DNS.php
@@ -1,62 +1,58 @@
 <?
-#####################################################
-## 					 BeatRock				   	   ##
-#####################################################
-## Framework avanzado de procesamiento para PHP.   ##
-#####################################################
-## InfoSmart © 2012 Todos los derechos reservados. ##
-## http://www.infosmart.mx/						   ##
-#####################################################
-## http://beatrock.infosmart.mx/				   ##
-#####################################################
+/**
+ * BeatRock
+ *
+ * Framework para el desarrollo de aplicaciones web.
+ *
+ * @author 		Iván Bravo <webmaster@infosmart.mx> @Kolesias123
+ * @copyright 	InfoSmart 2013. Todos los derechos reservados.
+ * @license 	http://creativecommons.org/licenses/by-sa/2.5/mx/  Creative Commons "Atribución-Licenciamiento Recíproco"
+ * @link 		http://beatrock.infosmart.mx/
+ * @version 	3.0
+ *
+ * @package 	DNS
+ * Permite la verificación de información por medio de DNS.
+ *
+*/
 
-// Acción ilegal.
-if(!defined('BEATROCK'))
+# Acción ilegal.
+if ( !defined('BEATROCK') )
 	exit;
 
 class DNS
 {
-	// Verificar si un dominio existe.
-	// - $domain: Dominio web.
+	/**
+	 * Verifica si un dominio existe.
+	 * @param string $domain Dominio
+	 */
 	static function CheckDomain($domain)
 	{
-		if(!Core::Valid($domain, 'domain'))
+		# No es un dominio válido.
+		if ( !Core::Valid($domain, DOMAIN) )
 			return false;
 
-		if(function_exists('checkdnsrr'))
+		# Usemos la función checkdnsrr
+		if ( function_exists('checkdnsrr') )
 		{
-			if(checkdnsrr($domain . '.', 'MX'))
+			if ( checkdnsrr($domain . '.', 'MX') )
 				return true;
 
-			if(checkdnsrr($domain . '.', 'A'))
+			if ( checkdnsrr($domain . '.', 'A') )
 				return true;
 		}
-		else if(function_exists('exec'))
+		# Usemos la consola.
+		else if ( function_exists('exec') )
 		{
 			exec('nslookup -type=A ' . $domain, $result);
 
-			foreach($result as $line)
+			foreach ( $result as $line )
 			{
-				if(Contains($domain, $line))
+				if ( Contains($domain, $line) )
 					return true;
 			}
 		}
 
 		return false;
-	}
-
-	// Verificar si un correo electrónico existe.
-	// - $email: Correo electrónico.
-	static function CheckEmail($email)
-	{
-		if(!Core::Valid($email))
-			return false;
-
-		$sender = 'kolesias17@gmail.com';
-		$SMTP 	= new SMTP_validateEmail();
-
-		$result = $SMTP->validate([$email], [$sender]);
-		return $result;
 	}
 }
 ?>
